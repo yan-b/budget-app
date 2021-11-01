33403aa726a59352769474b03dd54658e206b2ca
Yannick <yannick.buettler@gmx.de>
2021-11-01 17:33:19 +0100

    Changes in Endpoints

--- src/main/java/htw/berlin/budgetapp/config/ViewNames.java
+++ src/main/java/htw/berlin/budgetapp/config/ViewNames.java
@@ -3,6 +3,7 @@ package htw.berlin.budgetapp.config;
 public class ViewNames {
 
     public static final String INDEX = "index";
+    public static final String SLASH_INDEX = "slashIndex";
     public static final String ACCOUNTS = "accounts";
     public static final String CREATEACCOUNT = "newAccount";
     public static final String ENTRIES = "entries";
